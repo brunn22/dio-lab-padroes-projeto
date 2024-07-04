1f4e07ad5b2d5d0eab15eeb4bad1d553ed64e9da
Bruno Gomes <brunnolikeme@hotmail.com>
2024-07-04 19:27:25 -0300

    commit padrões de projetos

---
+++ lab-padroes-projetos/src/one/digitalinnovation/Test.java
@@ -0,0 +1,56 @@
+package one.digitalinnovation;
+
+import one.digitalinnovation.facade.Facade;
+import one.digitalinnovation.singleton.SingletonEager;
+import one.digitalinnovation.singleton.SingletonLazy;
+import one.digitalinnovation.singleton.SingletonLazyHolder;
+import one.digitalinnovation.strategy.Comportamento;
+import one.digitalinnovation.strategy.ComportamentoAgressivo;
+import one.digitalinnovation.strategy.ComportamentoDefensivo;
+import one.digitalinnovation.strategy.ComportamentoNormal;
+import one.digitalinnovation.strategy.Robo;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        // Singleton
+
+        SingletonLazy lazy = SingletonLazy.getInstancia();
+        System.out.println(lazy);
+        lazy = SingletonLazy.getInstancia();
+        System.out.println(lazy);
+
+        SingletonEager eager = SingletonEager.getInstancia();
+        System.out.println(eager);
+        eager = SingletonEager.getInstancia();
+        System.out.println(eager);
+
+        SingletonLazyHolder lazyHolder = SingletonLazyHolder.getInstancia();
+        System.out.println(lazyHolder);
+        lazyHolder = SingletonLazyHolder.getInstancia();
+        System.out.println(lazyHolder);
+
+        // Strategy
+
+        Comportamento defensivo = new ComportamentoDefensivo();
+        Comportamento normal = new ComportamentoNormal();
+        Comportamento agressivo = new ComportamentoAgressivo();
+
+        Robo robo = new Robo();
+        robo.setComportamento(normal);
+        robo.mover();
+        robo.mover();
+        robo.setComportamento(defensivo);
+        robo.mover();
+        robo.setComportamento(agressivo);
+        robo.mover();
+        robo.mover();
+        robo.mover();
+
+        // Facade
+
+        Facade facade = new Facade();
+        facade.migrarCliente("Bruno", "14801788");
+    }
+}
